5e0ace3da2e72cb2c2b7e63b6d2082ef0b1eca27
ParagNukte <paragnuktek@gmail.com>
2025-04-06 14:46:09 +0530

    Fixed linked for ReluAssignment

--- Portfolio-Parag/src/assistcomponents/ProjectData.js
+++ Portfolio-Parag/src/assistcomponents/ProjectData.js
@@ -10,7 +10,7 @@ export const ProjectData = [
     title: "Web Page Template",
     desc: "I have developed the web pae template from teh wireframe designed. I have used ReactJs, TailwindCss for styling.",
     image: "WebDesign.png",
-    live: "https://megablog-beta.vercel.app/",
+    live: "https://youtu.be/iE9l0ZT4P5Q?si=onZSo_3h44fUWXCm",
     github:
       "https://github.com/ParagNukte/assignments/tree/main/relu-consultancy-coding-round",
   },
